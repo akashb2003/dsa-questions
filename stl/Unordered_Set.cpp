015da4eb95b3530662442ddfad8aeefca840b041
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-09-12 19:12:43 +0530

    basics of  Unordered_Set

---
+++ stl/Unordered_Set.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // everything is same as set but stores does not store in ordered way
+    // lower_bound and upper_bound functions does not work
+    // rest all functions are same
+    // it has better time complexity than set in most cases - O(1)
+    // except some when collision happens - O(n)
+    
+    cout<<"Unordered Sets";
+    
+    unordered_set<int> ut;
+    
+    return 0;
+}
