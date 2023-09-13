c8f81872c88bb67a57029228ebb3f4de3b5c25ab
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-09-13 12:49:46 +0530

    Q1

--- Basic Math Problems/Q1.cpp
+++ Basic Math Problems/Q1.cpp
@@ -4,7 +4,7 @@
 //     output: 3, because 3 and 6 both can divide 336 hence 3 is counted twice
 //     n = 35
 //     output: 1, because in 3 and 5 only 5 can divide 35 evenly, only 5 is counted
-
+// O(log10(n))
 int countDigits(int n){
 	// Write your code here.
 	int cnt =0,rem=0;
