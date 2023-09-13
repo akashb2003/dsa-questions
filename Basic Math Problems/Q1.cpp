59c5f04c551eb713aefdd2abaee2ac29a5e034dd
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-09-13 12:47:38 +0530

    Create Q1.cpp

---
+++ Basic Math Problems/Q1.cpp
@@ -0,0 +1,22 @@
+//Find the number of digits 'n' that evenly divide the n
+// Ex: 
+//     n = 336
+//     output: 3, because 3 and 6 both can divide 336 hence 3 is counted twice
+//     n = 35
+//     output: 1, because in 3 and 5 only 5 can divide 35 evenly, only 5 is counted
+
+int countDigits(int n){
+	// Write your code here.
+	int cnt =0,rem=0;
+	int num = n;
+	while(num>0){
+		rem = num%10;
+
+		if(rem!=0 && n%rem==0){
+			cnt++;
+		}
+
+		num = num/10;
+	}	
+	return cnt;
+}
