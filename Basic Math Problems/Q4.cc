7dbca5f1043252ac19bd078f0f1066079af5f113
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-09-13 14:00:57 +0530

    Q4

---
+++ Basic Math Problems/Q4.cc
@@ -0,0 +1,21 @@
+//prnit all the divisors of a number
+
+vector<int> printDivisors(int n) {
+
+    // Write your code here
+
+    vector<int> ans;
+
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ans.push_back(i);
+            if (i != n / i)
+                ans.push_back(n / i);a
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+
+    return ans;
+
+}
