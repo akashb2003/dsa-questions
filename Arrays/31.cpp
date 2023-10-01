28470a5eeb150093ed11926854471166e3b27ba6
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-10-01 12:08:28 +0530

    Longest Consecutive Sequence

--- Arrays/31.cpp
+++ Arrays/31.cpp
@@ -1,5 +1,4 @@
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-
 // You must write an algorithm that runs in O(n) time.
 
 int longestSuccessiveElements(vector<int>&a) {
