80f238ea6bb8bd754a79cfcbbd5fc58d457aa942
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2023-10-11 17:15:50 +0530

    Search Insert Position

--- Binary Search/4.cpp
+++ Binary Search/4.cpp
@@ -36,3 +36,31 @@ public:
         return ans;
     }
 };
+
+//         OR          //
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+         int low = 0;
+         int high = nums.size() - 1;
+         
+         while (high >= low) {
+             int mid = low + (high - low) / 2;
+             
+             if (target == nums[mid]) {
+                 return mid;
+             }
+             else if (target > nums[mid]) {
+                 low = mid + 1;
+             }
+             else {
+                 high = mid - 1;
+             }
+         }
+         
+         
+         return low;
+    }
+};
