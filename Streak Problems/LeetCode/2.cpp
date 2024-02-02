3d45d56e3b266b6f0689fe9dad9f72c08102521e
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-02-02 14:24:34 +0530

    Sequential Digits

---
+++ Streak Problems/LeetCode/2.cpp
@@ -0,0 +1,28 @@
+// An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+// Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+// Example 1:
+
+// Input: low = 100, high = 300
+// Output: [123,234]
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        int a[] =  {12, 23, 34, 45, 56, 67, 78, 89, 123, 234,
+                    345, 456, 567, 678, 789, 1234, 2345, 3456, 
+                    4567, 5678, 6789, 12345, 23456, 34567, 45678,
+                    56789, 123456, 234567, 345678, 456789, 1234567, 
+                    2345678, 3456789, 12345678, 23456789, 123456789
+                    };
+        for(int i = 0; i< 36; i++){
+            if(a[i]<low || a[i]>high) continue;
+            ans.push_back(a[i]);
+        }
+        return ans;
+    }
+};
