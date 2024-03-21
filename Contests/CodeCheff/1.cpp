27d213fc85a6231d9a7656b9e3c493e18ba037ec
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-03-21 23:25:30 +0530

    AC Please

---
+++ Contests/CodeCheff/1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int temperature;
+    cin >> temperature;
+    if (temperature > 30) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
