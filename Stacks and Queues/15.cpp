4677dd535789f7b133949cfa8f1501e35320bd0c
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-04-05 13:12:09 +0530

    Next Smaller Element

---
+++ Stacks and Queues/15.cpp
@@ -0,0 +1,41 @@
+// You are given an array 'ARR' of integers of length N. Your task is to find the next smaller element for each of the array elements.
+
+// Next Smaller Element for an array element is the first element to the right of that element which has a value strictly smaller than that element.
+
+// If for any array element the next smaller element does not exist, you should print -1 for that array element.
+
+// For Example:
+
+// If the given array is [ 2, 3, 1], we need to return [1, 1, -1]. Because for  2, 1 is the Next Smaller element. For 3, 1 is the Next Smaller element and for 1, there is no next smaller element hence the answer for this element is -1.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints:
+// 1 <= T <= 10
+// 1 <= N <= 10 ^ 5
+// 0 <= ARR [i] <= 10 ^ 9
+
+// Time Limit: 1sec.
+// Sample Input 1:
+// 2
+// 4
+// 2 1 4 3
+// 3
+// 1 3 2
+// Sample Output 1:
+// 1 -1 3 -1
+// -1 2 -1
+
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n){
+    stack<int> s;
+    vector<int> ans(n);
+    s.push(-1);
+    for(int i = n-1; i >= 0; i--){
+        int curr = arr[i];
+        while(s.top() >= curr ){
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    return ans;
+}
