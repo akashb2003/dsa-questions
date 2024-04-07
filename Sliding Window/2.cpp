7cf88e8bd827133617d583799940ce58b496518f
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-04-07 07:46:24 +0530

    Max Consecutive Ones III

---
+++ Sliding Window/2.cpp
@@ -0,0 +1,30 @@
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0,r=0,zeros = 0, len=0;
+        int ans = 0;
+
+        while(r<nums.size()){
+            if(nums[r]==0) zeros++;
+            if(zeros>k){
+                if(nums[l]==0) zeros--;
+                l++;
+            }
+            if(zeros<=k) len = r-l+1;
+            ans = max(ans,len);
+            r++;
+        }
+        return ans;
+    }
+};
