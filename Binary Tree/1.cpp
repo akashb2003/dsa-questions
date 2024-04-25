2e97d539b17bfc40a1bacb6c5a23ef95d23b0270
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-04-25 07:45:09 +0530

    Number Of Nodes

---
+++ Binary Tree/1.cpp
@@ -0,0 +1,16 @@
+// Given an integer 'N', determine the maximum number of nodes present on 'Nth' level in a binary tree.
+
+
+
+// For Example:
+
+// N = 3
+// For level 1, we have 1 node.
+// For level 2, we have 2 nodes.
+// For level 3, we have 4 nodes
+// Output = 4, as level 3 has maximum 4 nodes.
+
+
+int numberOfNodes(int N){
+    return pow(2,N-1);
+}
