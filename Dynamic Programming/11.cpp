fa2c0c7af5e94c7064757882af9bb1386bad3d55
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-09-25 13:42:42 +0530

    Frog jump 2 / minimal cost

--- Dynamic Programming/11.cpp
+++ Dynamic Programming/11.cpp
@@ -1,26 +1,68 @@
-// There are n stones and an array of heights and Geek is standing at stone 1 and can jump to one of the following: Stone i+1, i+2, ... i+k stone, where k is the maximum number of steps that can be jumped and cost will be [hi-hj] is incurred, where j is the stone to land on. Find the minimum possible total cost incurred before the Geek reaches Stone n.
+// There is an array of heights corresponding to 'n' stones. You have to reach from stone 1 to stone ‘n’.
 
-// Examples :
-// Input: n = 5, k = 3 heights = {10, 30, 40, 50, 20}
-// Output: 30
-// Explanation: Geek will follow the path 1->2->5, the total cost would be | 10-30| + |30-20| = 30, which is minimum
+// From stone 'i', it is possible to reach stones 'i'+1, ‘i’+2… ‘i’+'k' , and the cost incurred will be | Height[i]-Height[j] |, where 'j' is the landing stone.
 
-class Solution {
-  public:
-    int minimizeCost(vector<int>& arr, int n, int k) {
-        // Code here
-        vector<int>dp(n);
-        dp[0]=0;
-        
-        for(int i=1; i<n; i++){
-            int minEg = INT_MAX;
-            for(int j=1;j<=k; j++){
-                if(i-j<0) break;
-                int cost = dp[i-j]+ abs(arr[i]-arr[i-j]);
-                minEg = min(minEg, cost);
-            }
-            dp[i] = minEg;
+// Return the minimum possible total cost incurred in reaching the stone ‘n’.
+
+// Example:
+// For 'n' = 3 , 'k' = 1, height = {2, 5, 2}.
+
+// Answer is 6.
+
+
+int help(int ind, int K, vector<int> &arr) {
+    if (ind == 0) return 0;
+    int mini = INT_MAX;
+
+    for (int k = 1; k <= K; k++) {
+        if (ind - k >= 0) { 
+            int cost = help(ind - k, K, arr) + abs(arr[ind] - arr[ind - k]);
+            mini = min(mini, cost);
         }
-        return dp[n-1];
     }
-};
+    return mini;
+}
+
+int minimizeCost(int n, int K, vector<int> &arr) {
+    return help(n - 1, K, arr); 
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int help(int ind, int K, vector<int> &arr,vector<int>&dp) {
+    if (ind == 0) return 0;
+    if(dp[ind]!= -1) return dp[ind];
+    
+    int mini = INT_MAX;
+
+    for (int k = 1; k <= K; k++) {
+        if (ind - k >= 0) { 
+            int cost = help(ind - k, K, arr,dp) + abs(arr[ind] - arr[ind - k]);
+            mini = min(mini, cost);
+        }
+    }
+    return dp[ind]=mini;
+}
+
+int minimizeCost(int n, int K, vector<int> &arr) {
+    vector<int>dp(n+1,-1);
+    return help(n - 1, K, arr,dp); 
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int minimizeCost(int n, int K, vector<int> &arr) {
+    vector<int>dp(n,0);
+    
+    for(int ind=1;ind<n;ind++){
+        int mini = INT_MAX;
+        for (int k = 1; k <= K; k++) {
+          if (ind - k >= 0) {
+            int cost = dp[ind - k] + abs(arr[ind] - arr[inad - k]);
+            mini = min(mini, cost);
+          }
+        }
+        dp[ind] = mini;
+    }
+    return dp[n-1];
+}
